87f210b5abfceddea00ea268d8a0a32283e47d8d
Dave <davidnjau21@gmail.com>
2024-09-11 09:31:00 +0300

    Added dynamic components

---
+++ app/src/main/java/com/intellisoftkenya/a24cblhss/dynamic_components/DataClass.kt
@@ -0,0 +1,56 @@
+package com.intellisoftkenya.a24cblhss.dynamic_components
+
+data class DbField(
+    val widgets: String,
+    val label: String,
+    val isMandatory: Boolean = false,
+
+    val inputType: Int? = null, // InputType for EditText (e.g., InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS)
+    val optionList : List<String> = emptyList()
+)
+
+data class DbFormData(
+    val tag: String,
+    val text: String,
+)
+
+data class DbCounty(
+    val id: Int,
+    val name: String
+)
+data class DbSubCounty(
+    val id: Int,
+    val name: String
+)
+
+enum class DbWidgets{
+    EDIT_TEXT, SPINNER
+}
+
+
+
+
+enum class DbEditTextNames {
+    FIRST_NAME_C, LAST_NAME_C, MIDDLE_NAME, OTHER_NAMES, TELEPHONE_C
+}
+enum class DbDOB {
+    YEARS_C, MONTHS, DAYS
+}
+enum class IdentificationTypes {
+    PASSPORT, NEMIS, NATIONAL_ID
+}
+
+data class FormData(
+    val firstName: String?,
+    val lastName: String?,
+    val middleName: String?,
+    val otherNames: String?,
+    val telephone: String?,
+    val years: Int?,
+    val months: Int?,
+    val days: Int?,
+    val selectedDate: String?,
+    val identificationType: IdentificationTypes?,
+    val number: Int?
+)
+
---
+++ app/src/main/java/com/intellisoftkenya/a24cblhss/dynamic_components/WidgetCreators.kt
@@ -0,0 +1,85 @@
+package com.intellisoftkenya.a24cblhss.dynamic_components
+
+import android.content.Context
+import android.graphics.Color
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.intellisoftkenya.a24cblhss.R
+
+/**
+ * This contains all the dynamic Widgets
+ */
+
+// Concrete implementation for label customization (OCP)
+class DefaultLabelCustomizer : LabelCustomizer {
+    override fun applyCustomization(
+        textView: TextView
+    ) {
+        // Ensure that the TextView has layoutParams
+        val layoutParams = textView.layoutParams ?: ViewGroup.MarginLayoutParams(
+            ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        ) .apply {
+            setMargins(8,8,8,0)
+        }
+
+        textView.layoutParams = layoutParams
+        textView.textSize = 18f
+
+        // Set text color and padding
+        textView.setTextColor(Color.BLACK)
+        textView.setPadding(16, 16, 16, 16)
+    }
+}
+
+// Concrete implementation for creating an EditText field
+class EditTextFieldCreator(
+    private val context: Context
+) : FieldCreator {
+    override fun createField(
+        label: String,
+        isMandatory: Boolean,
+        inputType: Int
+    ): View {
+        val editText = EditText(context).apply {
+            this.hint = label
+            this.inputType = inputType
+            this.background = ContextCompat.getDrawable(context, R.drawable.rounded_edittext) // Set rounded border
+            this.tag = label
+
+            this.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            ).apply {
+                setMargins(8,5,8,12)
+            }
+            this.setPadding(32,16,20,16)
+        }
+        return editText
+    }
+}
+
+// Concrete implementation for creating a Spinner field (LSP)
+class SpinnerFieldCreator(
+    private val options: List<String>,
+    private val context: Context) : FieldCreator {
+    override fun createField(
+        label: String,
+        isMandatory: Boolean,
+        inputType: Int): View {
+        val spinner = Spinner(context)
+        spinner.tag = label
+        val adapter = ArrayAdapter(context, R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        return spinner
+    }
+}
